8b71c91765ab4996ec7d0341b23c6c08b533edfd
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2025-01-05 07:15:41 +0100

    nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import svelte from "@astrojs/svelte"
 // https://astro.build/config
 export default defineConfig({
   site: "https://tom-mate-o.github.io",
-  base: "/min-max-calculator-nested/",
+  base: "/min-max-calculator-nested",
   integrations: [svelte()],
 })
